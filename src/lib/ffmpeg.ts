98dba858babb62f1762a0fcb74675fb2e3d852cc
Tatiana Narumi Matumoto <arumi.matsumoto@gmail.com>
2023-09-14 17:08:32 -0300

    feature: ffmpeg configuration

---
+++ src/lib/ffmpeg.ts
@@ -0,0 +1,25 @@
+import { FFMmpeg } from "@ffmpeg/ffmpeg";
+
+import coreURL from "../ffmpeg/ffmpeg-core.js?url";
+import wasmURL from "../ffmpeg/ffmpeg-core.wasm?url";
+import workerURL from "../ffmpeg/ffmpeg-worker?url";
+
+let ffmpeg: FFMmpeg | null;
+
+export async function getFFmpeg() {
+	if (ffmpeg) {
+		return ffmpeg;
+	}
+
+	ffmpeg = new FFMmpeg();
+
+	if (!ffmpeg.loaded) {
+		await ffmpeg.load({
+			coreURL,
+			wasmURL,
+			workerURL,
+		});
+	}
+
+	return ffmpeg;
+}
