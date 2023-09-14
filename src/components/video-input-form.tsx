128230348f0ee3a4069747fe63a4f68f8be869d1
Tatiana Narumi Matumoto <arumi.matsumoto@gmail.com>
2023-09-14 17:41:32 -0300

    ffmpeg

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -5,6 +5,7 @@ import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
 import { ChangeEvent, useState, useMemo, FormEvent, useRef } from "react";
 import { getFFmpeg } from "@/lib/ffmpeg";
+import { fetchFile} from '@ffmpeg/util';
 
 export function VideoInputForm() {
 	const [videoFile, setVideoFile] = useState<File | null>(null);
@@ -22,7 +23,34 @@ export function VideoInputForm() {
 		setVideoFile(selectedFile);
 	}
 
-    
+    async function convertVideoToAudio(video: File){
+        console.log('Convert started');
+
+        const ffmpeg = await getFFmpeg();
+
+        await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+
+        // ffmpeg.on('log', log => {
+        //     console.log(log)
+        // })
+
+        ffmpeg.on('progress', progress => {
+            console.log('Convert progress: ' + Math.round(progress.progress * 100))
+        })
+
+        await ffmpeg.exec([
+            '-i', 
+            'input.mp4',
+            '-map',
+            '0:a',
+            '-b:a',
+            '20k',
+            '-acodec',
+            'libmp3lame',
+            'output.mp3'
+        ])
+    }
+
 
 	function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
 		event.preventDefault();
